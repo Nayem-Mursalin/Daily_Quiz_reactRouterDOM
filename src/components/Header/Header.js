530a93c4dfc54422a79d04ddae9eca9c726c184d
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-10-12 13:28:15 +0600

    Add Nav Section

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <Link to='/'>Quiz</Link>
+                <Link to='/statistics'>Statistics</Link>
+                <Link to='/blog'>Blog</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
