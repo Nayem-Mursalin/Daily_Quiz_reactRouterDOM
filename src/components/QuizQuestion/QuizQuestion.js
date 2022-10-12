511fc64b5b9d2fb88866fb8f2eaa87eb145a28ba
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-10-12 22:32:28 +0600

    Eye icon Added

--- src/components/QuizQuestion/QuizQuestion.js
+++ src/components/QuizQuestion/QuizQuestion.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './QuizQuestion.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
 
 const QuizQuestion = ({ qs, id }) => {
     const { question, correctAnswer, options, rightAns, mistake } = qs;
@@ -26,7 +28,7 @@ const QuizQuestion = ({ qs, id }) => {
     return (
         <div className='quiz-question'>
             <h4>Question: {id} {question}</h4>
-            <button onClick={() => showAns(correctAnswer)}>See Ans</button>
+            <button className='eye' onClick={() => showAns(correctAnswer)}><FontAwesomeIcon icon={faEye}></FontAwesomeIcon></button>
             <ToastContainer />
             <div className='quiz-ans'>
 
